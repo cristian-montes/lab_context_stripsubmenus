853ffe4959aabf6eed3e9f6ce746f3fff7c5f2a3
Cristian Montes <cmontesmujica@gmail.com>
2021-11-19 15:12:54 -0700

    submenu displaying navbar titles

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const { isSubmenuOpen, location } = useGlobalContext();
+  const { isSubmenuOpen, location, page:{page, links} } = useGlobalContext();
   const container = useRef(null);
 
   useEffect(() => {
@@ -14,7 +14,7 @@ const Submenu = () => {
 
   return (
     <aside className={`${isSubmenuOpen ? 'submenu show ' : 'submenu'}`} ref={container}>
-      submenu
+      <h4>{page}</h4>
     </aside>
   )
 }
